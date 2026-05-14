0e6d33fe38368a02c764f1307d65958e3e742134
alexanderberille-alt <alexanderberille@gmail.com>
2026-05-14 16:39:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -382,7 +382,7 @@ async function getAIResponse(userId, userMessage, channel) {
     systemPrompt += `\n\n--- Connaissances ---\n${botConfig.knowledge.filter(Boolean).join('\n')}`;
   }
 
-  const ragContext = retrieveRelevantChunks(userMessage);
+  const ragContext = retrieveRelevantChunks(userMessage, 1, 2000);
   if (ragContext) {
   systemPrompt += `\n\n--- Extraits pertinents ---${ragContext}`;
 }
